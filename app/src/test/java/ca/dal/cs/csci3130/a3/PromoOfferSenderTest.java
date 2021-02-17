e0af32b53b829da75189f02fb509ed0034cb6bed
jolmstead <jl260629@dal.ca>
2021-02-17 19:14:51 -0400

    added Mockito setup for getExpiredDate() and getOffer(). ()

--- app/src/test/java/ca/dal/cs/csci3130/a3/PromoOfferSenderTest.java
+++ app/src/test/java/ca/dal/cs/csci3130/a3/PromoOfferSenderTest.java
@@ -1,9 +1,14 @@
 package ca.dal.cs.csci3130.a3;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.text.ParseException;
+
 import ca.dal.cs.csci3130.a3.q1.IPromoOffer;
 import ca.dal.cs.csci3130.a3.q1.PromoOfferSender;
 import static org.junit.Assert.assertEquals;
@@ -12,10 +17,18 @@ import static org.junit.Assert.assertEquals;
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class PromoOfferSenderTest {
 
-    static IPromoOffer offer = null;
+
+    static IPromoOffer offer = Mockito.mock(IPromoOffer.class);
+
+    @BeforeClass
+    public static void setup(){
+        Mockito.when(offer.getOffer()).thenReturn("This is a cool offer!"); //mockito for getOffer()
+        Mockito.when(offer.getExpiredDate()).thenReturn("2021-03-31"); //mockito for getExpiredDate();
+    }
+
 
     @Test
-    public void testSendOffer() {
+    public void testSendOffer() throws ParseException {
         PromoOfferSender sender = new PromoOfferSender();
         sender.sendOffer(offer);
         assertEquals("This is a cool offer!", offer.getOffer());
