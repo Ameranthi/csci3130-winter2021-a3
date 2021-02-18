779daae9c1fe70fa83d5c6191f777b3a51aa5878
jolmstead <jl260629@dal.ca>
2021-02-17 20:07:59 -0400

    refractor: perishable method moved to subclass vegetable; override get price in clothes and veg; moved the methods that assess if made in factory or farm to clothes and veg respectively

--- app/src/main/java/ca/dal/cs/csci3130/a3/q3/Clothes.java
+++ app/src/main/java/ca/dal/cs/csci3130/a3/q3/Clothes.java
@@ -2,4 +2,12 @@ package ca.dal.cs.csci3130.a3.q3;
 
 public class Clothes extends Product {
 
+    public boolean isProducedInAFactory() {
+        return true;
+    }
+
+    @Override
+    public int getPrice() {
+        return 20;
+    }
 }
--- app/src/main/java/ca/dal/cs/csci3130/a3/q3/Vegetable.java
+++ app/src/main/java/ca/dal/cs/csci3130/a3/q3/Vegetable.java
@@ -1,5 +1,15 @@
 package ca.dal.cs.csci3130.a3.q3;
 
 public class Vegetable extends Product {
+    public boolean isPerishable() {
+        return true;
+    }
+    public boolean isProducedInAFarm() {
+        return true;
+    }
 
+    @Override
+    public int getPrice() {
+        return 5;
+    }
 }
